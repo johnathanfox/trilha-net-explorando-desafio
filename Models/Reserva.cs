f2bba4555203793809ba5202601cee06b3f4781a
Johnathan Martins <johnathanfoxandrade@gmail.com>
2025-06-19 17:31:32 -0300

    "Testes de capacidade máxima e do desconto do mesmo efetuado com sucesso diaria 10d aplicando 10% de desc"

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -32,24 +32,17 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;          //retorna quantas pessoas estão hospedadas 
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
-            }
+                valor -= valor * 0.10m; //Desconto 10%
+            }   
 
             return valor;
         }
