9abdfcc17e20cc40160367f3e1df47b3c0984c75
Johnathan Martins <johnathanfoxandrade@gmail.com>
2025-06-19 17:01:49 -0300

    "Implementando validação de capacidade na classe Reserva"

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,9 +15,7 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (hospedes.Count <= Suite.Capacidade)    //Verifica se a quantidade de hóspedes é menor ou igual á capacidade da suite
             {
                 Hospedes = hospedes;
             }
